34e3efc37eba4e4c74294c5cca4905efb4691820
divy soni <chetanshudha267@gmail.com>
2024-05-22 11:05:16 +0530

    modified kakukatli code

--- Clang/kakjukatli.c
+++ Clang/kakjukatli.c
@@ -1,49 +1,92 @@
-#include<stdio.h>
+// #include<stdio.h>
+
+// void main(){
+//     int i,j,n;
+
+//     //scanning size or number of lines
+//     printf("Enter size : ");
+//     scanf("%d",&n);
+
+//     for(i=0;i<n;i++){
+//           for(j =0; j<=(n-i); j++){
+//                 printf("*");
+//             }
+
+//             //inner loop for stars
+//             for(j =1; j<=i ; j++){
+//                printf(" ");
+//             }
+
+//             //inner loop for stars
+//             for(j =1; j<=i ; j++){
+//                 printf(" ");
+//             }
+
+//             for(j=0;j<=(n-i);j++){
+//                 printf("*");
+//             }
+//         printf("\n");
+//     }
+
+//     for(i=n;i>0;i--){
+//         for(j =0; j<=(n-i) ; j++){
+//             printf("*");
+//         }
+
+//         //inner loop for stars
+//         for(j =1; j<=i ; j++){
+//             printf(" ");
+//         }
+
+//         //inner loop for stars
+//         for(j =1; j<=i ; j++){
+//             printf(" ");
+//         }
+
+//         for(j=0;j<=(n-i);j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+#include <stdio.h>
 
 void main(){
-    int i,j,n;
+    int i, j, n;
 
-    //scanning size or number of lines
-    printf("Enter size : ");
-    scanf("%d",&n);
+    // Scanning size or number of lines
+    printf("Enter size: ");
+    scanf("%d", &n);
 
-    for(i=0;i<n;i++){
-          for(j =0; j<=(n-i); j++){
-                printf("*");
-            }
-
-            //inner loop for stars
-            for(j =1; j<=i ; j++){
-               printf(" ");
-            }
-
-            //inner loop for stars
-            for(j =1; j<=i ; j++){
-                printf(" ");
-            }
-
-            for(j=0;j<=(n-i);j++){
-                printf("*");
-            }
+    // Top half of the pattern
+    for(i = 0; i < n; i++){
+        // Printing leading stars
+        for(j = 0; j < n - i; j++){
+            printf("*");
+        }
+        // Printing spaces
+        for(j = 0; j < 2 * i; j++){
+            printf(" ");
+        }
+        // Printing trailing stars
+        for(j = 0; j < n - i; j++){
+            printf("*");
+        }
         printf("\n");
     }
 
-    for(i=n;i>0;i--){
-        for(j =0; j<=(n-i) ; j++){
+    // Bottom half of the pattern
+    for(i = n - 1; i >= 0; i--){
+        // Printing leading stars
+        for(j = 0; j < n - i; j++){
             printf("*");
         }
-
-        //inner loop for stars
-        for(j =1; j<=i ; j++){
+        // Printing spaces
+        for(j = 0; j < 2 * i; j++){
             printf(" ");
         }
-
-        //inner loop for stars
-        for(j =1; j<=i ; j++){
-            printf(" ");
-        }
-
-        for(j=0;j<=(n-i);j++){
+        // Printing trailing stars
+        for(j = 0; j < n - i; j++){
             printf("*");
         }
         printf("\n");
