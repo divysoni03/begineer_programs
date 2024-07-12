fd28b21ef0b6351c63748de03dc86a5bc905af49
divy soni <chetanshudha267@gmail.com>
2024-07-12 09:27:29 +0530

    added size

--- Clang/fibonacci.c
+++ Clang/fibonacci.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 
 int main(){
-    int a=0,b=1;
+    int a=0,b=1,size;
+    printf("Enter range to be printed :");
+    scanf("%d",&size);
     printf("%d, %d, ",a,b);
-    for(int i=2;i<10;i++){
+    for(int i=2;i<size;i++){
         int c = a+b;
         printf("%d, ",c);
         a = b;
